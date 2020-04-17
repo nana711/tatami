09615ee5e28821ee0d9232044be148d6b1b34274
nana711 <58258939+nana711@users.noreply.github.com>
2020-04-17 14:46:36 +0900

    Add files via upload

---
+++ contact/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * TransmitMail
+ *
+ * @package   TransmitMail
+ * @license   MIT License
+ * @copyright TAGAWA Takao, dounokouno@gmail.com
+ * @link      https://github.com/dounokouno/TransmitMail
+ */
+
+require_once 'lib/TransmitMail.php';
+$tm = new TransmitMail();
+$tm->init('config/config.yml');
+$tm->run();
