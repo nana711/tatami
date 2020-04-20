69344371efc0804129ec7e1d328f7697497504b2
nana711 <58258939+nana711@users.noreply.github.com>
2020-04-20 20:30:52 +0900

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -24,3 +24,16 @@ $(document).ready(function () {
 });
 
 // スライダー
+
+$(window).scroll(function () {
+  $('.fadein').each(function () {
+    var elemPos = $(this).offset().top,
+      scroll = $(window).scrollTop(),
+      windowHeight = $(window).height();
+    if (scroll > elemPos - windowHeight + 100) {
+      $(this).addClass('scrollin');
+    }
+  });
+});
+
+// スクロール
