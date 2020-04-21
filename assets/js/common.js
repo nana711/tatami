628ddd9f7ea5f689046c4fc1e842bd3228deb7a7
nana711 <58258939+nana711@users.noreply.github.com>
2020-04-21 17:34:50 +0900

    Add files via upload

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,39 +1,36 @@
 $(function () {
   $(".Header--hams").on("click", function () {
-    $(this).toggleClass("clicked");
+    $(".Header--hams,.grobalNav").toggleClass("clicked");
+  });
+  $(".grobalNav_list--item").on("click", function () {
+    $(".Header--hams.clicked,.grobalNav.clicked").removeClass("clicked");
+  });
 
-    if ($(this).hasClass("clicked")) {
-      $(".grobalNav").addClass("clicked"); //クラスを付与
-    } else {
-      $(".grobalNav").removeClass("clicked"); //クラスを外す
-    }
+  // ハンバーガー
+  $(document).ready(function () {
+    $(".slider").slick({
+      arrows: false,
+      autoplay: true,
+      /* ポイントここから～ */
+      autoplaySpeed: 0,
+      cssEase: "linear",
+      speed: 20000,
+      /* ～ここまで */
+      slidesToScroll: 2,
+    });
+  });
+
+  // スライダー
+
+  $(window).scroll(function () {
+    $('.fadein').each(function () {
+      var elemPos = $(this).offset().top,
+        scroll = $(window).scrollTop(),
+        windowHeight = $(window).height();
+      if (scroll > elemPos - windowHeight + 100) {
+        $(this).addClass('scrollin');
+      }
+    });
   });
 });
-// ハンバーガー
-$(document).ready(function () {
-  $(".slider").slick({
-    arrows: false,
-    autoplay: true,
-    /* ポイントここから～ */
-    autoplaySpeed: 0,
-    cssEase: "linear",
-    speed: 20000,
-    /* ～ここまで */
-    slidesToScroll: 2,
-  });
-});
-
-// スライダー
-
-$(window).scroll(function () {
-  $('.fadein').each(function () {
-    var elemPos = $(this).offset().top,
-      scroll = $(window).scrollTop(),
-      windowHeight = $(window).height();
-    if (scroll > elemPos - windowHeight + 100) {
-      $(this).addClass('scrollin');
-    }
-  });
-});
-
 // スクロール
